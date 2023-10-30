b5ddbac846a4cd63f515597344388c8aab835b74
Alexisql <alexiskinte@gmail.com>
2023-10-30 15:31:52 -0500

    Se enlaza la vista con DataBinding.

--- app/src/main/java/com/alexis/data_binding/MainActivity.kt
+++ app/src/main/java/com/alexis/data_binding/MainActivity.kt
@@ -2,10 +2,23 @@ package com.alexis.data_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.alexis.data_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView(){
+        binding.apply {
+            name = "Alexis"
+            lastName = "Quintero"
+        }
     }
 }
